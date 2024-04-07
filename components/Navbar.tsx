077b75f69722e5673666cbacee7a6d1709e6f354
hakeem-jc <hakeemclarke14@gmail.com>
2024-04-07 09:39:26 +0200

    Add desktop navbar title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+        <Link href='/' className='flex items-center gap-1'>
+        <Image
+            src='/icons/logo.svg'
+            width={32}
+            height={32}
+            alt='Xoom Logo'
+            className='max-sm:size-10'
+          />
+          <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Xoom</p>
+        </Link>
+      </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
