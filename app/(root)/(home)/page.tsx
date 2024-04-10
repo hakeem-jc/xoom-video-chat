1941f8145f93af285f9ef75d9b51f7413aec77b0
hakeem-jc <hakeemclarke14@gmail.com>
2024-04-10 12:03:52 +0200

    Add dynamic time and date

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const Home = () => {
+  const now  = new Date();
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit'});
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
@@ -8,9 +12,11 @@ const Home = () => {
           <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meeting at: 12:30 PM</h2>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold'>
-              11:30 AM
+              {time}
             </h1>
-            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>Wednesday, April 10, 2024</p>
+            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>
+              {date}
+            </p>
           </div>
         </div>
       </div>
