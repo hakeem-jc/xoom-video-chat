883ef2e7b4d98c820504db00a690c72fafbada18
hakeem-jc <hakeemclarke14@gmail.com>
2024-06-12 14:34:00 -0500

    (feat) Add Add join by meeting invite

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,7 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useToast } from "@/components/ui/use-toast";
 import ReactDatePicker from 'react-datepicker';
 import { Textarea } from './ui/textarea';
+import { Input } from './ui/input';
 
 const MeetingTypeList = () => {
   const router  = useRouter();
@@ -152,6 +153,21 @@ const MeetingTypeList = () => {
             buttonText="Start Meeting"
             handleClick={createMeeting}
         />
+
+        <MeetingModal
+            isOpen={meetingState === 'isJoiningMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title="Type the link here"
+            className="text-center"
+            buttonText="Join Meeting"
+            handleClick={() => router.push(values.link)}
+        >
+            <Input
+                placeholder="Meeting link"
+                onChange={(e) => setValues({ ...values, link: e.target.value })}
+                className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+            />
+        </MeetingModal>
     </section>
   )
 }
