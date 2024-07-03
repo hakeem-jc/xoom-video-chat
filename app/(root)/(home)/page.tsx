44ac66af67dd456fc71a5fd3dd79e7a5950aa31a
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-03 10:29:29 -0500

    refactor: Format page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,33 +1,36 @@
-import MeetingTypeList from '@/components/MeetingTypeList';
+import MeetingTypeList from "@/components/MeetingTypeList";
 import { currentUser } from "@clerk/nextjs";
 
-const Home = async() => {
-  const now  = new Date();
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit'});
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+const Home = async () => {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
   const user = await currentUser();
 
   return (
-    <section className='flex size-full flex-col gap-10 text-white'>
+    <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-          {user !== null &&
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Hi {user?.firstName || 'there'}</h2>
-          }
-          <div className='flex flex-col gap-2'>
-            <h1 className='text-4xl font-extrabold'>
-              {time}
-            </h1>
-            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>
-              {date}
-            </p>
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          {user !== null && (
+            <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+              Hi {user?.firstName || "there"}
+            </h2>
+          )}
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
 
       <MeetingTypeList />
     </section>
-  )
-}
+  );
+};
 
 export default Home;
