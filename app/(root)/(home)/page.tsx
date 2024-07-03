67f2e2b6035252ac398f7a0640ccca586c94f20b
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-03 10:35:51 -0500

    feat: Update time card design

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -14,15 +14,10 @@ const Home = async () => {
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          {user !== null && (
-            <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-              Hi {user?.firstName || "there"}
-            </h2>
-          )}
+      <div className="flex justify-center items-center h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold">{time}</h1>
+            <h1 className="text-5xl font-extrabold">{time}</h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
